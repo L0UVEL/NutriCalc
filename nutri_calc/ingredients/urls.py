9353319d65132ca43c75e2b4b5491998400654e9
= <=>
2026-04-16 09:20:33 +0800

    Fix Add Bugs

--- nutri_calc/ingredients/urls.py
+++ nutri_calc/ingredients/urls.py
@@ -4,8 +4,8 @@ from ingredients import views
 urlpatterns = [
     # CRUD
     path('', views.IngredientListView.as_view(), name='ingredient_list'),
-    path('ingredient/<str:pk>/', views.ingredient_detail, name='ingredient_detail'),
     path('ingredient/add/', views.ingredient_create, name='ingredient_create'),
+    path('ingredient/<str:pk>/', views.ingredient_detail, name='ingredient_detail'),
     path('ingredient/<str:pk>/edit/', views.ingredient_update, name='ingredient_update'),
     path('ingredient/<str:pk>/delete/', views.ingredient_delete, name='ingredient_delete'),
 ]
--- nutri_calc/recipes/urls.py
+++ nutri_calc/recipes/urls.py
@@ -3,8 +3,8 @@ from recipes import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
-    path('recipe/<str:pk>/', views.recipe_detail, name='recipe_detail'),
     path('recipe/create/', views.recipe_create, name='recipe_create'),
+    path('recipe/<str:pk>/', views.recipe_detail, name='recipe_detail'),
     path('recipe/<str:pk>/edit/', views.recipe_update, name='recipe_update'),
     path('recipe/<str:pk>/delete/', views.recipe_delete, name='recipe_delete'),
 ]
